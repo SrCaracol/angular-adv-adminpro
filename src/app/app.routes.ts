1dd552224a625f1dc7a093659bf7e847fc485552
SrCaracol <albeldocarlitos@gmail.com>
2024-09-16 20:02:49 +0200

    fin seccion 3 diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficalComponent } from './pages/grafical/grafical.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'progress', component: ProgressComponent },
+  { path: 'grafical', component: GraficalComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent },
+];
---
+++ src/app/pages/grafical/grafical.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafical',
+  standalone: true,
+  imports: [],
+  templateUrl: './grafical.component.html',
+  styles: ``
+})
+export class GraficalComponent {
+
+}
